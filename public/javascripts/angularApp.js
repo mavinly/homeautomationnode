0dcb044f1a85fc62c777153d3478f38b6aa7694c
unknown <cmurphy@PC04016.hdna.hd.lan>
2015-12-07 15:20:49 -0700

    Deleting file and add a readings

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,7 +1,10 @@
 var app = angular.module('flapperNews', []);
 
-app.controller('MainCtrl', [
-'$scope',
-function($scope){
+app.controller('MainCtrl', ['$scope', function($scope){
   $scope.test = 'Hello world!';
+  $scope.readings = [
+    'reading 1',
+    'reading 2',
+    'reading 3'
+  ];
 }]);
