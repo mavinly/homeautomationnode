1f7b39e29eae4f48f0d12c7dbcde5667faf98eec
unknown <cmurphy@PC04016.hdna.hd.lan>
2015-12-09 15:55:14 -0700

    Adding angular routes

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,14 +1,38 @@
-var app = angular.module('doorSensorReadings', []);
+var app = angular.module('doorSensorReadings', ['ui.router']);
 
-app.controller('MainCtrl', ['$scope', function($scope){
-  $scope.readings = [
-    {id: 1, name:"Door 1", open: true},
-    {id: 1, name:"Door 2", open: true},
-    {id: 1, name:"Door 3", open: true},
-  ];
+app.factory('readings', [function(){
+  var o = {
+    readings: []
+  };
+  return o;
+}])
+
+app.controller('MainCtrl', ['$scope', 'readings', function($scope, readings){
+  $scope.readings = readings.readings;
+  
+  // $scope.readings = [
+  //   {id: 1, name:"Door 1", open: true},
+  //   {id: 1, name:"Door 2", open: true},
+  //   {id: 1, name:"Door 3", open: true},
+  // ];
   
   $scope.addReading = function(){
     $scope.readings.push({id: 2, name:"Door 4", open: true})
   }
   
+}]);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
 }]);
