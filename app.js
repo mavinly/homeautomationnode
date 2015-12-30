68d0ca19bcbbdf625bf0e491f08f59978e44e67d
unknown <cmurphy@PC04016.hdna.hd.lan>
2015-12-30 15:46:35 -0700

    Adding security

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var passport = require('passport');
 require('./models/doorreadings');
+require('./models/Users');
+require('./config/passport');
 mongoose.connect('mongodb://admin:ecmj0803@ds054298.mongolab.com:54298/sample');
 
 
@@ -25,6 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
